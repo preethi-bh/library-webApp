232a9a17fed161a0013b51630970dc59e522e322
AdItYa <preethi.marple@gmail.com>
2017-06-22 15:39:11 +0530

    modified:   src/main/java/rest/Application.java

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -1,5 +1,6 @@
 package rest;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application
