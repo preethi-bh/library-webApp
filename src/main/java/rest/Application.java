77531f0fb60f8973afdd79dde7753309e2298cf5
AdItYa <preethi.marple@gmail.com>
2017-06-22 15:27:42 +0530

    new file:   src/main/java/rest/Application.java 	modified:   target/classes/rest/Controller.class

---
+++ src/main/java/rest/Application.java
@@ -0,0 +1,10 @@
+package rest;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application
+{
+	public static void main(String args[]) throws Exception{
+		SpringApplication.run(Application.class,args);
+	}
+}
