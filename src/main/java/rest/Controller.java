97be6fb8c8531d0c51f40579f301726b8dc153b6
AdItYa <preethi.marple@gmail.com>
2017-06-22 16:51:31 +0530

    modified:   src/main/java/rest/Controller.java

--- src/main/java/rest/Controller.java
+++ src/main/java/rest/Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class Controller{
 	Service service=new Service();
 	
-	@RequestMapping(value="/search",method=RequestMethod.POST,produces=MediaType=APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/search",method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<Model> getBook(@RequestParam String username,@RequestParam String bname){
 		List<Model> m=service.getBookAvail(username,bname);
 		return m;
